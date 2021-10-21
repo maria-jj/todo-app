27a420a365a05199a4df173ba3955ed42bd28100
maria-jj <maryamjafar@cmail.carleton.ca>
2021-10-21 19:23:52 -0400

    deleting todos

--- OneDrive/Desktop/fall 2021/todoApp/todo-app/src/components/new-todo-form/index.jsx
+++ OneDrive/Desktop/fall 2021/todoApp/todo-app/src/components/new-todo-form/index.jsx
@@ -1,12 +1,28 @@
 import {useForm} from 'react-hook-form';
 import {Input} from '../input-field';
+import {v1 as uuidv1} from 'uuid';
+import {useContext} from 'react';
+import {TodosContext} from '../../context/todos-context';
+import {useHistory} from 'react-router-dom';
+
 import './styles.css'
 export const NewTodoForm =()=>{
-const {register, handleSubmit, formState: {errors}}=useForm();
+    const todoContext = useContext(TodosContext);
+    let history = useHistory();
 
-const onSubmit = (data)=>{
-    console.log(data)
+    const {register, handleSubmit, formState: {errors}}=useForm();
+
+    const onSubmit = (data)=>{
+    const todo = data;
+    todo.id = uuidv1();
+    todo.isComplete=false;
+    todoContext.addTodo(todo);
+    console.log("added new todo",todo)
+
+    //navigate to home page
+    history.push("/");
 }
+
 return(
     <form onSubmit={handleSubmit(onSubmit)}>
         {}
--- OneDrive/Desktop/fall 2021/todoApp/todo-app/src/components/todo/index.jsx
+++ OneDrive/Desktop/fall 2021/todoApp/todo-app/src/components/todo/index.jsx
@@ -1,19 +1,29 @@
 import PropTypes from "prop-types";
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {TodosContext} from '../../context/todos-context'
 import {GoTrashcan, GoCheck} from 'react-icons/go';
 
 import './styles.css'
 export const Todo = (props) => {
-
+    const todosContext = useContext(TodosContext);
     const [isComplete, setIsComplete]=useState(false);
 
     useEffect( () => {
         setIsComplete(props.isComplete)
-    }, [props.isComplete])
+    }, []);
+
+    useEffect( () =>{
+        setIsComplete(props.todoId, isComplete)
+    }, [isComplete]);
+    
    
     const toggleCompleteTodo = ()=>{
         setIsComplete(!isComplete);
 
+    }
+
+    const deleteTodo = () =>{
+        todosContext.deleteTodo(props.todoId);
     }
 
     return (
@@ -27,7 +37,7 @@ export const Todo = (props) => {
                     <GoCheck className="todo-icon" style={{fontSize:"35px"}}/>
                 </button>
             </div>
-            <div ClassName="todo-delete">
+            <div ClassName="todo-delete" onClick={(deleteTodo)}>
                 <button ClassName="todo-complete">
                 <GoTrashcan className="todo-icon" style={{fontSize:"35px"}}/>
 
